fd74b5134db42401270b4339a43f98fe8b62434b
Mike Harder <mharder@microsoft.com>
2020-07-06 16:38:47 -0700

    Add proxy settings in script comment

--- g2/run.ps1
+++ g2/run.ps1
@@ -1,3 +1,4 @@
 #!/usr/bin/env pwsh
 
+# -Dhttp.proxyHost=127.0.0.1 -Dhttp.proxyPort=8888 -Dhttps.proxyHost=127.0.0.1 -Dhttps.proxyPort=8888 -Djava.net.useSystemProxies=true
 java -jar target/java-datalake-startup-perf-g2-1.0-SNAPSHOT-jar-with-dependencies.jar $args
