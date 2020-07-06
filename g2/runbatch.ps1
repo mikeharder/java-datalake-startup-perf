e7416a7fab1383cc29602096fa87bd367e616d6f
Mike Harder <mharder@microsoft.com>
2020-07-06 16:38:56 -0700

    Script to run batch of tests

---
+++ g2/runbatch.ps1
@@ -0,0 +1,12 @@
+#!/usr/bin/env pwsh
+
+pwsh run.ps1 1 aad
+pwsh run.ps1 1 sharedkey
+pwsh run.ps1 1 aad
+pwsh run.ps1 1 sharedkey
+pwsh run.ps1 1 aad
+pwsh run.ps1 1 sharedkey
+pwsh run.ps1 1 aad
+pwsh run.ps1 1 sharedkey
+pwsh run.ps1 1 aad
+pwsh run.ps1 1 sharedkey
