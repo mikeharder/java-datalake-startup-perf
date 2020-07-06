c0bbb16bd0a4e1de6679738824ffab972a34e12b
Mike Harder <mharder@microsoft.com>
2020-07-06 13:01:36 -0700

    Flow command-line args

--- g2/run.ps1
+++ g2/run.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env pwsh
 
-java -jar target/java-datalake-startup-perf-g2-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/java-datalake-startup-perf-g2-1.0-SNAPSHOT-jar-with-dependencies.jar $args
